b4f682a158c534d673898f25f2cc806139857e19
uzmakhanam1 <167199140+uzmakhanam1@users.noreply.github.com>
2024-04-18 16:43:03 +0530

    Add files via upload

---
+++ pt1.py
@@ -0,0 +1,13 @@
+'''if(5>10):
+    print("yes")
+else:
+    print("no")'''
+#conditiona
+age1=int(input("enter age1"))
+age2=int(input("enter age2"))
+if(age1>age2):
+    age3=age1+age2
+    print('my age added',age3)
+else:
+    age3=age1-age2
+    print('my age subracted',age3)
